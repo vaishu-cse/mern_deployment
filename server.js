6aa07e3158f9f2ec2aa531ed43e8020a28254e12
vaishu_cse <vaishnavikcse@gmail.com>
2025-10-29 14:34:50 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://vaishnavi_k_mongo:uCyuekUVonTtsLnY@cluster0.gm5sebb.mongodb.net/?appName=Cluster0')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Schema and Model
+const userSchema = new mongoose.Schema({ name: String });
+const User = mongoose.model('User', userSchema);
+
+// Routes
+app.post('/add', async (req, res) => {
+  const user = new User({ name: req.body.name });
+  await user.save();
+  res.json({ message: 'Name stored successfully!' });
+});
+
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+// Start server
+app.listen(5000, () => console.log('Server running on port 5000'));
