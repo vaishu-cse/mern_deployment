747e5c9513a7bdd7580a71d4b8a251f7faea1c8d
vaishu_cse <vaishnavikcse@gmail.com>
2025-10-29 14:54:06 +0530

    feat: Added port from env

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.get('/users', async (req, res) => {
   res.json(users);
 });
 
-// Start server
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
